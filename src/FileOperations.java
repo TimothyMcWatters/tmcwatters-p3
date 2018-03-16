f5b6c6afe1fbb9f376c481b26c316c589e1bb6a7
[TimothyMcWatters] <timmch2o@gmail.com>
2018-03-15 21:02:13 -0500

    Initial commit

---
+++ src/FileOperations.java
@@ -0,0 +1,25 @@
+
+/**
+ * @author Timothy McWatters
+ * @version 1.0
+ * 
+ * COP 4027 Advanced Computer Programming
+ * Project 3
+ * File Name: FileOperations.java
+ * 
+ * This Program: Will create a graphical spell checker program
+ * It will have a menu item to open an input file and place it into a TextArea in a window. 
+ * When the checking proceeds, it will extract a word from the text to be checked, 
+ * hash it into the table, and determine if it exists. 
+ * You will continue this process until you have checked all the words in the file. 
+ * Each time you find a word that you cannot match in the dictionary, you will let the user 
+ * know and you will attempt to generate a list of suggested words. 
+ * You will generate the list by assembling similar words via three methods:
+ *		One letter missing. 
+ *		One letter added. 
+ *		Two letters reversed. 
+ */
+
+public class FileOperations {
+
+}
